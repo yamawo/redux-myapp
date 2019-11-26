60c1ec2f340942750608e48d28616398f5135629
yamawo <y7a3m1asaki@gmail.com>
2019-11-26 23:58:28 +0900

    create table. events_index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,5 @@ const QUERYSTRING = '?token=token123'
 
 export const readEvents = () => async dispatch => {
     const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
-    console.log(response);
     dispatch({ type: READ_EVENTS, response })
 }
--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import { readEvents } from '../actions'
 
@@ -7,18 +8,36 @@ const EventsIndex = (props) => {
 
   useEffect(() => {
       props.readEvents()
-  });
+  }, []);
+
+  const renderEvents = () => {
+    return _.map(props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
 
   return (
-    <>
-      <div>value: { props.value }</div>
-      <button onClick={props.increment}>+1</button>
-      <button onClick={props.decrement}>-1</button>
-    </>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Body</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {renderEvents()}
+      </tbody>
+    </table>
   );
 }
 
-const mapStateToProps = state => ({}) // このコンポーネントに必要なstateを抽出して、propsとしてマッピングする
+const mapStateToProps = state => ({ events: state.events }) // このコンポーネントに必要なstateを抽出して、propsとしてマッピングする
 const mapDispatchToProps = ({ readEvents })  // reducerにタイプに応じた状態遷移を行わせる
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
